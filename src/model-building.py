1d4c073af46fae33f65e64fcacad4d4a730b4c83
trandinhthang203 <tranthangkhuong203@gmail.com>
2025-08-22 08:42:32 +0700

    feat: modular code  model-build, model-eval

--- src/model-building.py
+++ src/model-building.py
@@ -1,23 +1,44 @@
 import pandas as pd
 import os
-import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 import yaml
 
-train_data = pd.read_csv(os.path.join('data', 'processed', 'train_processed.csv'))
-test_data = pd.read_csv(os.path.join('data', 'processed', 'test_processed.csv'))
+def get_train_data(filepath: str) -> tuple[pd.DataFrame]:
+    try:
+        df = pd.read_csv(filepath)
+        X_train = df.drop(columns=['Potability'], axis=1)
+        y_train = df['Potability']
+        return X_train, y_train
+    except Exception as e:
+        raise Exception(f'Error: ', e)
 
-# X_train = train_data.iloc[:,0:-1].values
-# y_train = train_data.iloc[:,-1].values
 
-X_train = train_data.drop(columns=['Potability'], axis=1)
-y_train = train_data['Potability']
+def get_n_estimators(filepath: str) -> int:
+    try:
+        return yaml.safe_load(open(filepath))['model-building']['n_estimators']
+    except Exception as e:
+        raise Exception(f'Error: ', e)
+    
 
-n_estimators = yaml.safe_load(open('params.yaml'))['model-building']['n_estimators']
-print('n_estimators: ', n_estimators)
-clf = RandomForestClassifier(n_estimators = n_estimators)
-clf.fit(X_train, y_train)
+def training_model(n_estimators: int, X_train: pd.DataFrame, y_train: pd.DataFrame) -> None:
+    try:
+        clf = RandomForestClassifier(n_estimators=n_estimators)
+        clf.fit(X_train, y_train)
+        pickle.dump(clf, open('random_forest_model.pkl', 'wb'))
+    except Exception as e:
+        raise Exception(f'Error: ', e)
+    
 
-pickle.dump(clf, open('random_forest_model.pkl', 'wb'))
-
+def main():
+    try:
+        DATA_PATH = os.path.join('data', 'processed', 'train_processed.csv')
+        X_train, y_train = get_train_data(DATA_PATH)
+        n_estimators = get_n_estimators('params.yaml')
+        training_model(n_estimators, X_train, y_train)
+    except Exception as e:
+        raise Exception(f'Error: ', e)
+    
+    
+if __name__ == '__main__':
+    main()
--- src/model-eval.py
+++ src/model-eval.py
@@ -5,19 +5,39 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 import pickle
 import json
 
-test_data = pd.read_csv(os.path.join('data', 'processed', 'test_processed.csv'))
-X_test = test_data.iloc[:, 0:-1].values
-y_test = test_data.iloc[:, -1].values
+def get_data(filepath: str) -> tuple[pd.DataFrame, pd.DataFrame]:
+    try:
+        test_data = pd.read_csv(filepath)
+        X_test = test_data.iloc[:, 0:-1].values
+        y_test = test_data.iloc[:, -1].values
+        return X_test, y_test
+    except Exception as e:
+        raise Exception(f'Error: ', e)
 
-model = pickle.load(open('random_forest_model.pkl', 'rb'))
-y_pred = model.predict(X_test)
+def write_metrics(X_test: pd.DataFrame, y_test: pd.DataFrame) -> None:
+    try:
+        model = pickle.load(open('random_forest_model.pkl', 'rb'))
+        y_pred = model.predict(X_test)
 
-metrics = {
-    'accuracy': accuracy_score(y_test, y_pred),
-    'precision': precision_score(y_test, y_pred, average='weighted'),
-    'recall': recall_score(y_test, y_pred, average='weighted'),
-    'f1_score': f1_score(y_test, y_pred, average='weighted')
-}
+        metrics = {
+            'accuracy': accuracy_score(y_test, y_pred),
+            'precision': precision_score(y_test, y_pred, average='weighted'),
+            'recall': recall_score(y_test, y_pred, average='weighted'),
+            'f1_score': f1_score(y_test, y_pred, average='weighted')
+        }
+        with open('model_evaluation_metrics.json', 'w') as f:
+            json.dump(metrics, f, indent=4)
+    except Exception as e:
+        raise Exception(f'Error: ', e)
 
-with open('model_evaluation_metrics.json', 'w') as f:
-    json.dump(metrics, f, indent=4)
+
+def main():
+    DATA_PATH = os.path.join('data', 'processed', 'test_processed.csv')
+    try:
+        X_test, y_test = get_data(DATA_PATH)
+        write_metrics(X_test, y_test)
+    except Exception as e:
+        raise Exception(f'Error: ', e)
+    
+if __name__ == '__main__':
+    main()
